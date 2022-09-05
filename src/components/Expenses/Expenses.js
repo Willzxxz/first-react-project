c5141188ae7ca9cb61b42f3ccb04398ad3936f83
Willzxxz <willzxxz@gmail.com>
2022-09-05 00:44:57 -0300

    splitting and organizing files

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
   // const title = props.title();
   // const amount = props.amount();
   // const date = props.date();
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -28,7 +29,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 

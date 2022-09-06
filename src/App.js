cf92cc0fc7c321563c9876b4411534f801c5b44d
Willzxxz <willzxxz@gmail.com>
2022-09-05 23:33:48 -0300

    assignment2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -28,15 +29,19 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <div className="./components/Expenses.css">
         <Expenses items={expenses} />
       </div>
     </div>
   );
-}
-
+};
 
 export default App;
